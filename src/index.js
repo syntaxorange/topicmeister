8d669544eda75b2ec7cdc3188ca4c25b6e99dfec
syntaxorange <syntaxorange@gmail.com>
2022-12-24 22:31:10 +0300

    add add topic logic

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ class GetDropdown extends React.Component {
       isActive: false
     };
     this.handleClick = this.handleClick.bind(this);
+    this.handleItemClick = this.handleItemClick.bind(this);
   }
   
   componentDidMount() {
@@ -36,6 +37,11 @@ class GetDropdown extends React.Component {
     this.setState({ isActive: !this.state.isActive });
   }
 
+  handleItemClick() {
+    this.props.onClick();
+    this.handleClick();
+  }
+
   render() {
     return (
       <div className="dropdown">
@@ -43,7 +49,9 @@ class GetDropdown extends React.Component {
           <span className="material-icons material-icons-outlined">more_vert</span>
         </GetButton>
         <div className={`dropdown-content${this.state.isActive ? ' active' : ''}`}>
-          {this.props.children}
+          {this.props.items.map(item => { 
+            return <GetButton class="button-dropdown" text={item} onClick={this.handleItemClick}/>
+          })}
         </div>
       </div>
     );
@@ -53,13 +61,26 @@ class GetDropdown extends React.Component {
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isAddTopic: false,
+      currentTopic: '',
+      topics: ['React', 'Angular', 'Vue', 'JavaScript', 'Css', 'Html']
+    }
+    this.handleAddTopicClick = this.handleAddTopicClick.bind(this);
+    this.handleApplyTopicClick = this.handleApplyTopicClick.bind(this);
+    this.handleTopicInputChange = this.handleTopicInputChange.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('keyup', e => {
+      if (e.key === 'Enter' && this.state.currentTopic)
+        this.handleApplyTopicClick();
+    });
   }
 
   renderTopics() {
-    const names = ['React', 'Angular', 'Vue', 'JavaScript', 'Css', 'Html'];
-
     return (
-      names.map((value, index) => {
+      this.state.topics.map((value, index) => {
         return (
           <GetButton key={index} class="topic" text={value}>
             <span className="material-icons material-icons-outlined">keyboard_arrow_right</span>
@@ -69,22 +90,67 @@ class App extends React.Component {
     );
   }
 
+  renderAddTopic() {
+    if (this.state.isAddTopic) {
+      return (
+        <GetButton class="topic">
+          <input 
+            type="text" 
+            className="topic-input" 
+            onChange={this.handleTopicInputChange}
+            value={this.state.currentTopic}
+          />
+          <span className="material-icons material-icons-outlined" onClick={this.handleApplyTopicClick}>add_box</span>
+        </GetButton>
+      );
+    }
+  }
+
+  handleTopicInputChange(e) {
+    this.setState({
+      currentTopic: e.target.value
+    });
+  }
+
+  handleAddTopicClick() {
+    this.setState({
+      isAddTopic: !this.state.isAddTopic,
+      currentTopic: ''
+    }, () => {
+      if (this.state.isAddTopic)
+        document.querySelector('.topic-input').focus();
+    });
+  }
+
+  handleApplyTopicClick() {
+    const currentTopic = this.state.currentTopic;
+    const topics = this.state.topics;
+
+    if (!currentTopic || topics.some(v => v === currentTopic)) {
+      return;
+    }
+
+    this.setState({
+      topics: [currentTopic, ...topics],
+      currentTopic: ''
+    });
+  }
+
   render() {
     return (
       <div className="topic-meister">
         <div className="top">
           <div className="title">Topic Meister</div>
           <div className="top-icons">
-            <GetDropdown>
-              <GetButton class="button-dropdown" text="Add topic"/>
-              <GetButton class="button-dropdown" text="Change topic"/>
-              <GetButton class="button-dropdown" text="Remove topic"/>
-            </GetDropdown>
-            <GetButton class="add">
+            <GetDropdown 
+              items={['Add topic', 'Change topic', 'Remove topic']}
+              onClick={this.handleAddTopicClick}/>
+            <GetButton class="add" onClick={this.handleAddTopicClick}>
               <span className="material-symbols-outlined">add</span>
             </GetButton>
           </div>
         </div>
+        {this.renderAddTopic()}
         {this.renderTopics()}
       </div>
     );
