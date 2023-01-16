b6e8fb423dc6c5eaaae5660b2ab18bb467744811
syntaxorange <syntaxorange@gmail.com>
2023-01-16 22:23:58 +0300

    add switch concept on click

--- src/chrome/content.js
+++ src/chrome/content.js
@@ -1,11 +1,48 @@
 import storage from "../storage";
+
 /* eslint-disable */
 const storageKey = 'topic_meister_topics';
+const playTimeout = 10000;
 let timerId = 0;
+let index = 0;
+
 const removeTopic = () => {
   const existingTopic = document.getElementById('topic_meister_topic');
   if (existingTopic)
     existingTopic.remove();
+}
+
+const switchConcept = topics => {
+  if (!Object.keys(topics).length) {
+    clearTimeout(timerId);
+    return;
+  }
+  const concepts = getConcepts(topics);
+  
+  if (!concepts.length) {
+    clearTimeout(timerId);
+    return;
+  }
+  index = getPlayingConceptIndex(concepts);
+  if (index === concepts.length - 1) {
+    index = 0;
+    concepts[concepts.length - 1].playing = false;
+  } else {
+    index += 1;
+    concepts[index - 1].playing = false;
+  }
+  
+  addTopic({title: concepts[index].title, content: concepts[index].content});
+  playConcept(topics, concepts[index]);
+}
+
+const runTimer = () => {
+  timerId = setTimeout(function tick() {
+    storage.get(storageKey).then(topics => {
+      switchConcept(topics);
+      timerId = setTimeout(tick, playTimeout); 
+    });
+  }, playTimeout);
 }
 
 const addTopic = ({title, content}) => {
@@ -44,6 +81,14 @@ const addTopic = ({title, content}) => {
   topic.appendChild(topicContent);
   topic.appendChild(topicTitle);
   body.appendChild(topic);
+
+  topic.addEventListener('click', () => {
+    clearTimeout(timerId);
+    storage.get(storageKey).then(topics => {
+      switchConcept(topics);
+      runTimer();
+    });
+  });
 }
 
 const playConcept = (topics, concept) => {
@@ -72,63 +117,37 @@ const getPlayingConceptIndex = concepts => {
   return concepts.findIndex(o => o.playing);
 }
 
-storage.get(storageKey).then(topics => {
-  if (!Object.keys(topics).length)
-    return;
-  const concepts = getConcepts(topics);
-  console.log('%c⧭', 'color: #d0bfff', concepts);
- 
-  if (!concepts.length)
-    return;
-  const index = getPlayingConceptIndex(concepts);
-  playConcept(topics, concepts[index]);
-});
-
-const runTimer = () => {
-  const playTimeout = 10000;
-  let index = 0;
-  timerId = setTimeout(function tick() {
-    storage.get(storageKey).then(topics => {
-      if (!Object.keys(topics).length) {
-        clearTimeout(timerId);
-        return;
-      }
-      const concepts = getConcepts(topics);
-      index = getPlayingConceptIndex(concepts);
-  
-      if (!concepts.length) {
-        clearTimeout(timerId);
-        return;
-      }
-      if (index === concepts.length - 1) {
-        index = 0;
-        concepts[concepts.length - 1].playing = false;
-      } else {
-        index += 1;
-        concepts[index - 1].playing = false;
-      }
-      
-      console.log('%c⧭', 'color: #99614d', concepts);
-      addTopic({title: concepts[index].title, content: concepts[index].content});
-      playConcept(topics, concepts[index]);
-      timerId = setTimeout(tick, playTimeout); 
-    });
-  }, playTimeout);
-}
-runTimer();
-chrome.runtime.onMessage.addListener(({ id, play }, sender, sendResponse) => {
-  console.log('%c⧭', 'color: #514080', sendResponse);
+const init = () => {
   storage.get(storageKey).then(topics => {
-    const concepts = getConcepts(topics, true);
-
-    if (play && concepts.length === 1) {
-      playConcept(topics, concepts[0]);
-      clearTimeout(timerId);
-      sendResponse();
-      runTimer();
-    }
-    if (!play)
-      stopConcept(topics, concepts.find(o => o.id === id));
+    if (!Object.keys(topics).length)
+      return;
+    const concepts = getConcepts(topics);
+    console.log('%c⧭', 'color: #d0bfff', concepts);
+   
+    if (!concepts.length)
+      return;
+    const index = getPlayingConceptIndex(concepts);
+    playConcept(topics, concepts[index]);
   });
-});
+  
+  runTimer();
+  
+  chrome.runtime.onMessage.addListener(({ id, play }, sender, sendResponse) => {
+    console.log('%c⧭', 'color: #514080', sendResponse);
+    storage.get(storageKey).then(topics => {
+      const concepts = getConcepts(topics, true);
+  
+      if (play && concepts.length === 1) {
+        playConcept(topics, concepts[0]);
+        clearTimeout(timerId);
+        sendResponse();
+        runTimer();
+      }
+      if (!play)
+        stopConcept(topics, concepts.find(o => o.id === id));
+    });
+  });
+}
+
+init();
 /* eslint-enable */
